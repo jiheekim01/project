736712e41056b2f69fe379d6290cb83167ce092e
jiheekim01 <jiheezang@gmail.com>
2025-06-20 21:49:37 +0900

    Add files via upload

---
+++ 03. 연락처 관리 프로그램 (Hashmap 사용)/main/contactMain.java
@@ -0,0 +1,9 @@
+package contactMVC.main;
+
+import contactMVC.control.serviceControl;
+
+public class contactMain {
+	public static void main(String[] args) {
+		serviceControl.contactControl("hashmap");
+	}
+}
