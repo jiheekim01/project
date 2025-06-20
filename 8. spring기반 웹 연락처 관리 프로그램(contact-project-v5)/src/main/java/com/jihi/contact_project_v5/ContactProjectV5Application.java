f581f4e7854df02a1942804d8a57fbf59551c6e4
jiheekim01 <jiheezang@gmail.com>
2025-06-20 21:41:44 +0900

    Add files via upload

---
+++ 8. spring기반 웹 연락처 관리 프로그램(contact-project-v5)/src/main/java/com/jihi/contact_project_v5/ContactProjectV5Application.java
@@ -0,0 +1,13 @@
+package com.jihi.contact_project_v5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactProjectV5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactProjectV5Application.class, args);
+	}
+
+}
