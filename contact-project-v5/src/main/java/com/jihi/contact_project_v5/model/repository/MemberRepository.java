acf8934309dde423f5930d46c39d48e163b27220
jiheekim01 <jiheezang@gmail.com>
2025-06-20 21:42:59 +0900

    Delete contact-project-v5 directory

--- contact-project-v5/src/main/java/com/jihi/contact_project_v5/model/repository/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jihi.contact_project_v5.model.repository;
-
-import com.jihi.contact_project_v5.model.member.MemberEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemberRepository extends CrudRepository<MemberEntity, String> { }
